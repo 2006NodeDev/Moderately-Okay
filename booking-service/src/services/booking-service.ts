887f69bc96559dad2e71011ab0b0ea22aa5eace6
ABlanton3 <a.m.blanton@gmail.com>
2020-08-03 14:59:03 -0500

    edited booking-service

--- booking-service/src/services/booking-service.ts
+++ booking-service/src/services/booking-service.ts
@@ -1,8 +1,8 @@
 
-//import { SaveTattooImage } from "../daos/CloudStorage/booking-images";
-//import { bucketBaseUrl } from "../daos/CloudStorage";
+import { SaveTattooImage } from "../daos/CloudStorage/booking-images";
+import { bucketBaseUrl } from "../daos/CloudStorage";
 import { Bookings } from "../models/Bookings";
-import { getAllBookings, updateExistingBooking, findBookingById, findBookingByCustomer } from "../daos/SQL/booking-dao";
+import { getAllBookings, findBookingById, findBookingByCustomer } from "../daos/SQL/booking-dao";
 
 // this call dao
 
@@ -16,27 +16,40 @@ export async function findBookingByCustomerService(userId:number):Promise<Bookin
 }
 
 // will work on in later feel free to comment out this method if you need to 
-/*export async function SubmitNewBookingService(newBooking:Bookings):Promise<Bookings>{
+export async function SubmitNewBookingService(newBooking:Bookings):Promise<Bookings>{
     
     try{
         let base64Image = newBooking.imageTest
         let [dataType, imageBase64Data] = base64Image.split(';base64,')
         let contentType = dataType.split('/').pop()
         if (newBooking.imageTest) {
-            newBooking.imageTest = `${bucketBaseUrl}/users/${newBooking.customer}/profile.${contentType}`
+            newBooking.imageTest = `${bucketBaseUrl}/bookings/${newBooking.bookingId}/profile.${contentType}` //this might still need some changes, but I think it should work
         }
-        let savedBooking =  await submitNewBooking(newBooking)
-        await SaveTattooImage(contentType, imageBase64Data, `users/${newBooking.customer}/profile.${contentType}`)
+        let savedBooking =  await SubmitNewBookingService(newBooking)
+        await SaveTattooImage(contentType, imageBase64Data, `bookings/${newBooking.bookingId}/profile.${contentType}`) //this might still need some changes, but I think it should work
         return savedBooking
     }catch (e){
         console.log(e)
         throw e 
     }
     
-}*/
+}
 
-export async function UpdateExistingBookingService(booking:Bookings):Promise<Bookings>{
-    return await updateExistingBooking(booking)
+export async function UpdateExistingBookingService(updatedBooking:Bookings):Promise<Bookings>{
+    try{
+        let base64Image = updatedBooking.imageTest
+        let [dataType, imageBase64Data] = base64Image.split(';base64,')
+        let contentType = dataType.split('/').pop()
+        if (updatedBooking.imageTest) {
+            updatedBooking.imageTest = `${bucketBaseUrl}/bookings/${updatedBooking.bookingId}/profile.${contentType}` //this might still need some changes, but I think it should work
+        }
+        let savedBooking =  await UpdateExistingBookingService(updatedBooking)
+        await SaveTattooImage(contentType, imageBase64Data, `bookings/${updatedBooking.bookingId}/profile.${contentType}`) //this might still need some changes, but I think it should work
+        return savedBooking
+    }catch (e){
+        console.log(e)
+        throw e 
+    }
 }
 
 
