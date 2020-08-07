c3071c30e1d0c0b3aa0918c4b0d47afca86176be
faithOnly <firdevssyildirimm@gmail.com>
2020-08-07 17:20:51 -0500

    update on remote index.ts

--- Moderately-Okay-UI/moderatelyokay-ui/src/remote/moderatelyokay-api/index.ts
+++ Moderately-Okay-UI/moderatelyokay-ui/src/remote/moderatelyokay-api/index.ts
@@ -5,6 +5,5 @@ export const modokayClient = axios.create({
     baseURL: 'http://localhost:2006' || 'http://localhost:2013',
     headers:{
         'Content-Type': 'application/json',
-        'Authorization' : 'Bearer '
     }
 })
